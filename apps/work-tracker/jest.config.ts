f334ef59baac773ac85f4641fc89d0af9f14d7fb
myin <minyin142p@gmail.com>
2022-05-12 16:37:35 +0200

    initial commit

---
+++ apps/work-tracker/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'work-tracker',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/work-tracker',
+};
