756cf95597ab77112088ce82165cfa39dfe153d9
myin <minyin142p@gmail.com>
2022-05-12 16:57:59 +0200

    refactor: change to more beautiful dialog

--- apps/work-tracker/src/app/app.tsx
+++ apps/work-tracker/src/app/app.tsx
@@ -7,7 +7,16 @@ export function App() {
 
   return (
     <div>
-      <button onClick={() => setWorkDialogOpen(true)}>Open</button>
+      <div className="fixed inset-0 flex items-center justify-center">
+        <button
+          type="button"
+          onClick={() => setWorkDialogOpen(true)}
+          className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+        >
+          Open dialog
+        </button>
+      </div>
+
       <WorkDialog
         open={workDialogOpen}
         onClose={() => setWorkDialogOpen(false)}
